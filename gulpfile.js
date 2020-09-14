7e668e51c823ebe0c2cd1860b408eb3714e25869
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2020-09-14 11:43:18 +0100

    remove competition pages
    darkblue will be building and hosting these pages separately

--- gulpfile.js
+++ gulpfile.js
@@ -97,9 +97,9 @@ function buildJsMain(cb) {
     './_assets/js/_components/modal.js',
     './_assets/js/_components/modal-nav.js',
     // './_assets/js/_components/sticky-nav.js',
-    './_assets/js/_components/form/functions.js',
-    './_assets/js/_components/form/validation.js',
-    './_assets/js/_components/competition.js',
+    // './_assets/js/_components/form/functions.js',
+    // './_assets/js/_components/form/validation.js',
+    // './_assets/js/_components/competition.js',
     './_assets/js/_components/offer-sheet.js',
     './_assets/js/_components/offer-countdown.js',
 
